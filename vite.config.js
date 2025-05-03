c094c3e71dd55d82500cbe38ace9aea9d37ba5b3
Paul Lee <paul_lee@zoho.com>
2025-05-03 10:03:14 -0400

    Updated configurations.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { resolve } from 'path'
+import vituum from 'vituum'
+import nunjucks from '@vituum/vite-plugin-nunjucks'
 
 export default {
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: '../dist'
+        outDir: '../dist',
+        rollupOptions: {
+            input: [
+                'index.njk.html',
+            ]
+        }
     },
     server: {
-        port: 8484
-    }
+        port: 8484,
+        hot: true
+    },
+    plugins: [
+        vituum(),
+        nunjucks()
+    ]
 }
